ca39e68804e4f576d50bbfaeafab907aa0b2e3ed
jjwebsolutions <jj.websolutions@proton.me>
2023-12-15 10:56:17 +0100

    reorg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
 import { ExpenseProvider } from "./ExpenseContext";
 import AddExpense from "./components/AddExpense";
 import ExpenseList from "./components/ExpenseList";
